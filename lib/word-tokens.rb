367bc82f3847fec23eeef1188f347fd3418ae61b
Jamie Murai <murai.jamie@gmail.com>
2011-01-06 18:01:40 -0500

    Added WordTokens class

--- lib/word-tokens.rb
+++ lib/word-tokens.rb
@@ -1,2 +1,11 @@
 class WordTokens
+  def self.token(num = 3, delimiter = "-", filter = [], path = "/usr/share/dict/words")
+    word_list = File.read(path).split
+    filter.each {|f| word_list.delete(f) }
+    list_size = word_list.length
+    
+    output = word_list[rand(list_size)]
+    (num - 1).times { output << "#{delimiter}#{word_list[rand(list_size)]}" }
+    output.downcase
+  end
 end
